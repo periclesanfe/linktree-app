36d2a427dfc794257931a97981617faae08da40c
periclesanfe <periclesanfe@gmail.com>
2026-01-15 01:13:45 -0300

    fix: corrigir a desestruturação da importação do authMiddleware

--- linktree-backend/src/routes/inviteCodes.js
+++ linktree-backend/src/routes/inviteCodes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db/pool');
-const { authMiddleware } = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 const logger = require('../utils/logger');
 
 // POST /api/invite-codes/validate - Validar código (público)
